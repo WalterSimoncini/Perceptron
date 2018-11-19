4a7b659784b56a7d1e14c64301231eedc56d9cd9
Walter Simoncini <walterdev@icloud.com>
2018-11-19 16:42:01 +0100

    Added bias node for handling of the AND function

--- perceptron.py
+++ perceptron.py
@@ -6,10 +6,13 @@ line_separator = "\n------------------------------------------------------------
 learning_constant = 0.1
 
 # Targets and inputs for the OR function
-targets = [0, 1, 1, 1] 
-inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+# targets = [0, 1, 1, 1] 
+# inputs = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
 
-weights = [random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)]
+targets = [0, 0, 0, 1]
+inputs = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+
+weights = [random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)]
 
 sum_error = 1000
 # prev_error_sum = error_sum
@@ -29,7 +32,7 @@ while sum_error > 0:
 
     for i in range(0, len(targets)):
         t = targets[i]
-        output = inputs[i][0] * weights[0] + inputs[i][1] * weights[1] + bias
+        output = inputs[i][0] * weights[0] + inputs[i][1] * weights[1] + inputs[i][2] * weights[2]
 
         activation = activation_function(output)
         error = t - activation
@@ -43,5 +46,6 @@ while sum_error > 0:
         print "Target: " + str(t) + " - Ouput: " + str(activation) + " - " + str(weights)
     
     print("\nEpoch " + str(epoch) + " with error sum: " + str(sum_error) + line_separator)
+    
     time.sleep(1)
     epoch += 1
